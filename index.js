7d7402597e7316f7da9f90c1c80b3f43b1ddb488
LAXMIDEVICH <151652845+LAXMIDEVICH@users.noreply.github.com>
2023-11-27 14:43:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,71 +1,57 @@
-const registrationForm=document.getElementById('registration-form');
-const userDataTable=document.getElementById('user-data');
-const userDataTableBody=userDataTable.querySelector('tbody');
-const dobInput=document.getElementById('dob');
-const dobError=document.getElementById('dobError');
-window.addEventListener('load',() =>{
-    updateUserDataTable();
-});
-registrationForm.addEventListener('submit',(event) =>{
-    event.preventDefault();
-    const userData= {
-        name:document.getElementById('name').value,
-        email:document.getElementById('email').value,
-        password:document.getElementById('password').value,
-        dob:document.getElementById('dob').value,
-        terms:document.getElementById('terms').checked
-    }:
-if(!validateUserDate(userData)){
-    const errorMessage=document.createElement('p');
-    errorMessage.textContent='Value must be 20/11/1967 or later';
-    errorMessage.classList.add('error-message');
-    const dateField=document.getElementById('dob');
-    dateField.parentNode.appendChild(errorMessage);
+import logo from "./logo.svg";
+import "./App.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Login from "./components/Login";
+import Lost from "./components/Lost";
+import AddLostItem from "./components/AddLostItem";
+import Found from "./components/Found";
+import AddFoundForm from "./components/AddFoundForm";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Register from "./components/Register";
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/",
+    element: (
+      <ProtectedRoute>
+        <Lost />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: "/addLostItem",
+    element: (
+      <ProtectedRoute>
+        <AddLostItem />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: "/found",
+    element: (
+      <ProtectedRoute>
+        <Found />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: "/addFoundItem",
+    element: (
+      <ProtectedRoute>
+        <AddFoundForm />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+]);
+function App() {
+  return <RouterProvider router={router} />;
+}
 
-}else{
-    saveUserData(userData);
-    updateUserDataTable();
-    clearForm();
-}
-});
-function validateUserData(userData){
-    const minAge=18;
-    const maxAge=55;
-    const today=new Date();
-    const birthDate=new Date(userData.dob);
-    const age=today.getFullYear() - birthDate.getFullYear();
-    if(age<minAge ||age>maxAge) {
-        return false;
-    }
-    return true;
-}
-function saveUserData(userData)
-{
-    const existingUserData=JSON.parse(localStorage.getItem('userList')) || [];
-    existingUserData.push(userData);
-    loalStorage.setItem('userList',JSON.stringify(existingUserData));
-}
-function updateUserDataTable()
-{
-userDataTableBody.innerHTML='';
-    const userList=JSON.parse(localStorage.getItem('userList')) || [];
-    userList.forEach((userData)=>{
-        const userDataRow=createUserDataTableRow(userData);
-        userDataTableBody.appendChild(userDataRow);
-    });
-    if(userList.length>0){
-        userDataTable.classList.remove('hidden');
-    }else{
-        userDataTable.classList.add('hidden');
-    }
-}
-function createUserDataTableRow(userData){
-    const row=document.createElement('tr');
-    row.innerHTML='<td>${userData.name}</td><td>${userData.email}</td><td>${userData.password}</td><td>${userData.dob}</td><td>${userData.terms ? 'true':'false'}</td>';
-    return row;
-}
-function clearForm() {
-registrationForm.reset();
-}
-    
-        
+export default App;
